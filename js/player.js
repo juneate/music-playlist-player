0668b72ef04c9a433699071dd174e6fa09a961cd
Rocco P <roccopan@gmail.com>
2021-12-15 17:11:03 -0500

    updated default

--- js/player.js
+++ js/player.js
@@ -13,9 +13,9 @@ let draggingProgress = false
 let indexPlaying = 0
 
 const trackDataToDom = [
-	{ id: `trackName`,		data: `name`,		prop: `textContent`,	default: `Track Name` },
-	{ id: `trackArtist`,	data: `artist`,	prop: `textContent`,	default: `Artist Name` },
-	{ id: `trackGenre`,		data: `genre`,	prop: `textContent`,	default: `Genre` },
+	{ id: `trackName`,		data: `name`,		prop: `textContent`,	default: `Unknown Name` },
+	{ id: `trackArtist`,	data: `artist`,	prop: `textContent`,	default: `Unknown Artist` },
+	{ id: `trackGenre`,		data: `genre`,	prop: `textContent`,	default: `` },
 	{ id: `trackText`,		data: `text`,		prop: `textContent`,	default: `` },
 	{ id: `trackImage`,		data: `img`,		prop: `src`,					default: `https://craftypixels.com/placeholder-image/400x400/e0e0e0/ffffff&text=Placeholder` },
 ]
